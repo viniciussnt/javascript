ac663fb2775c028bd4f8a1c5484fc7dfcdfb2067
Vinicius Santos <97640204+viniciussnt@users.noreply.github.com>
2022-10-17 10:11:42 -0300

    aprendendo condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 120
+console.log(`A velocidade do seu carro é ${vel}KM/h`)
+if (vel > 60) {
+    console.log(`Você atravessou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
